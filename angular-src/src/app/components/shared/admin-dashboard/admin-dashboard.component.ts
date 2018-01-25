527e985d81f797a03b4450473e7d15888dc6a5ba
Shakir Ghani <shakir.ghani@hotmail.com>
2018-01-25 12:54:49 +0500

    modals

--- angular-src/src/app/components/shared/admin-dashboard/admin-dashboard.component.ts
+++ angular-src/src/app/components/shared/admin-dashboard/admin-dashboard.component.ts
@@ -10,8 +10,21 @@ import { Router } from '@angular/router';
 })
 export class AdminDashboardComponent implements OnInit {
 user:any;
+data: Array<any> = [];
+  editItemsForm: boolean = false;
+  name : String;
+  username: String;
+  email: String;
+  password: String;
+  userType: String;
+  dob: String;
+  contactnum: number;
+  age: number;
+  gender: String;
+  _id: any;
   constructor(
-    private authService:AuthService
+    private authService:AuthService,
+    private router:Router
   ) { }
 
   ngOnInit() {
@@ -23,6 +36,38 @@ user:any;
       console.log(err);
       return false;
     });
+    this.authService.getAllProfile().subscribe((data) => {
+      console.log(data);
+      this.data = data.getData;
+    },
+    err => {
+      console.log(err);
+      return false;
+    });
   }
-
+  onUpdateUser(user){     
+    console.log(user, "_id")
+    var url = "update" + "/" + user._id
+    this.authService.updateUser(url, user).subscribe(
+      (data) => {
+        console.log(data, "fgdfg");
+        this.data = data.getData;
+        this.authService.getUser(user.username);
+        this.editItemsForm = false;
+        
+      },
+      (err) => {
+        return err
+      }
+    )
+  }
+  OnDeleteUser(_id, i: number){
+    this.authService.deleteUser(_id).subscribe(data=> {
+      this.data.splice(i, 1)
+      console.log(data,"data from db")
+    },
+    err => {
+      console.error(err, "error" )
+    }
+  )};
 }
