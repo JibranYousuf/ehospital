9a493c946a026ed09a208a8ae62715943069ee4d
Jibran Yousuf <jibran.ubit@gmail.com>
2018-01-29 00:23:38 +0500

    updated new features

--- angular-src/src/app/components/home/home.component.ts
+++ angular-src/src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { CanActivate } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-home',
