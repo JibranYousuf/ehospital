d140c46321dc3b146eba5664ee3f3487b34895c5
Jibran Yousuf <jibran.ubit@gmail.com>
2018-01-29 10:51:15 +0500

    new updated features

--- angular-src/src/app/components/shared/doctor-dashboard/doctor-dashboard.component.ts
+++ angular-src/src/app/components/shared/doctor-dashboard/doctor-dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AptService } from '../../../services/apt.service';
+import { Response } from '@angular/http/src/static_response';
+import { ValidateService } from '../../../services/validate.service'
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 
 @Component({
@@ -10,67 +14,115 @@ import { Router } from '@angular/router';
 })
 export class DoctorDashboardComponent implements OnInit {
   user:any;
+  appointment: any;
   data: Array<any> = [];
-    editItemsForm: boolean = false;
-    name : String;
-    username: String;
-    email: String;
-    password: String;
-    userType: String;
-    dob: String;
-    contactnum: number;
-    age: number;
-    gender: String;
-    _id: any;
-    dname: any;
-    aptname: any;
-    constructor(
-      private authService:AuthService,
-      private router:Router
-    ) { }
+  editItemsForm: boolean = false;
+  name : String;
+  drname: String;
+  aptname: String;
+  aptDateCreated: any;
+  aptDate: any;
+  aptStatus: String;
+  aptType: String;
+  contactnum: String;
+  selectedCourse: any;
+  constructor(
+    private validateService: ValidateService, 
+    private flashMessage:FlashMessagesService,
+    private authService:AuthService,
+    private aptService:AptService,
+    private router:Router
+  ) { }
   
-    ngOnInit() {
-      this.authService.getProfile().subscribe(profile => {
-        console.log(profile);
-        this.user = profile.user;
-      },
-      err => {
-        console.log(err);
-        return false;
-      });
-      this.authService.getAllProfile().subscribe((data) => {
-        console.log(data);
-        this.data = data.getData;
-      },
-      err => {
-        console.log(err);
-        return false;
-      });
-    }
-    onUpdateUser(user){     
-      console.log(user, "_id")
-      var url = "update" + "/" + user._id
-      this.authService.updateUser(url, user).subscribe(
-        (data) => {
-          console.log(data, "fgdfg");
-          this.data = data.getData;
-          this.authService.getUser(user.username);
-          this.editItemsForm = false;
-          
-        },
-        (err) => {
-          return err
-        }
-      )
-    }
-    OnDeleteUser(_id, i: number){
-      this.authService.deleteUser(_id).subscribe(data=> {
-        this.data.splice(i, 1)
-        console.log(data,"data from db")
-      },
-      err => {
-        console.error(err, "error" )
-      }
-    )};
+
+  ngOnInit() {
+    this.authService.getProfile().subscribe(profile => {
+      console.log(profile);
+      this.user = profile.user;
+    },
+    err => {
+      console.log(err);
+      return false;
+    }); 
+    this.authService.getAllProfile().subscribe((data) => {
+      console.log(data);
+      this.data = data.getData;
+    },
+    err => {
+      console.log(err);
+      return false;
+    });   
+  }
+
+  onView(){
+    this.authService.getAllProfile().subscribe((data) => {
+      console.log(data);
+      this.data = data.getData;
+    },
+    err => {
+      console.log(err);
+      return false;
+    });
+  }
+
+  onAppointmentView(){
+    this.aptService.getAllAppointments().subscribe((data) => {
+      console.log(data);
+      this.data = data.getAppointmentData;
+    },
+    err => {
+      console.log(err);
+      return false;
+    });
   }
   
+  onAppointmentSubmit(){
+    const appointment = {
+      name: this.name,
+      drname: this.drname,
+      aptStatus: this.aptStatus,
+      aptType: this.aptType,
+      aptDateCreated: this.aptDateCreated,
+      aptDate: this.aptDate,
+      contactnum: this.contactnum,
+    }
+    if(!this.validateService.validateAppointment(appointment)){
+      this.flashMessage.show('Please Fill in all fields', {cssClass: 'alert-danger', timeout: 3000})
+      return false;
+    }
+    
+    this.aptService.createApt(appointment).subscribe(data =>{
+      if(data.success){ 
+        this.router.navigate(['/dashboard']);
+      } else{    
+        this.router.navigate(['/profile']);
+      }
+    });
+  }
+
+  onUpdateUser(user){     
+    console.log(user, "_id")
+    var url = "update" + "/" + user._id
+    this.authService.updateUser(url, user).subscribe(
+      (data) => {
+        console.log(data, "fgdfg");
+        this.data = data.getData;
+        this.authService.getUser(user.username);
+        this.editItemsForm = false;
+        
+      },
+      (err) => {
+        return err
+      }
+    )
+  }
+  OnDeleteAppointment(_id, i: number){
+    this.aptService.deleteAppointment(_id).subscribe(data=> {
+      this.data.splice(i, 1)
+      console.log(data,"data from db")
+    },
+    err => {
+      console.error(err, "error" )
+    }
+  )};
+}
