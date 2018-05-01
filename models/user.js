84dd8e52a44d05f318ac2d9fba554be6ef274c4a
Bilal Alam <bilalalam173@gmail.com>
2018-05-01 16:34:30 +0500

    used fenago server

--- models/user.js
+++ models/user.js
@@ -1,86 +1,56 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const config = require('../config/database');
-
-// User Schema
-var Schema = mongoose.Schema;
-const UserSchema = Schema({
-  constName: {
-    type: String
-  },
-  constEmail: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  constUsername: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  constCnic: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  constUserType:{
-    type: String,
-    required: true,
-  },
-  constAddress:{
-    type: String,
-  },
-  constGender:{
-    type: String,
-  },
-  constAge:{
-    type: String,
-  },
-  constDob:{
-    type: String,
-  },
-  constContactNum:{
-    type: String,
-  },
-  constQualification:{
-    type: String,
-  },
-  constDesignation:{
-    type: String,
-  },
-  challan: [{
-    type: Schema.Types.ObjectId, ref: 'Challan'
-  }]
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
 });
-
-const User = module.exports = mongoose.model('User', UserSchema);
-
-module.exports.getUserById = function(id, callback){
-  User.findById(id, callback);
-}
-
-module.exports.getUserByCnic = function(constCnic, callback){
-  const query = {constCnic: constCnic}
-  User.findOne(query, callback);
-}
-
-module.exports.addUser = function(newUser, callback){
-  bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(newUser.password, salt, (err, hash) => {
-      if(err) throw err;
-      newUser.password = hash;
-      newUser.save(callback);
+var bcrypt = require("bcryptjs");
+var mongoose = require("mongoose");
+var userSchema = new mongoose.Schema({
+    username: String,
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: String,
+    role: String
+});
+// Before saving the user, hash the password
+userSchema.pre('save', function (next) {
+    var user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) {
+            return next(err);
+        }
+        bcrypt.hash(user.password, salt, function (error, hash) {
+            if (error) {
+                return next(error);
+            }
+            user.password = hash;
+            next();
+        });
     });
-  });
-}
-
-module.exports.comparePassword = function(candidatePassword, hash, callback){
-  bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
-    if(err) throw err;
-    callback(null, isMatch);
-  });
-}
+});
+userSchema.methods.comparePassword = function (candidatePassword, callback) {
+    console.log(candidatePassword + '-----' + this.password);
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        console.log(isMatch);
+        if (err) {
+            return callback(err);
+        }
+        callback(null, isMatch);
+    });
+};
+// Omit the password when returning a user
+userSchema.set('toJSON', {
+    transform: function (doc, ret, options) {
+        delete ret.password;
+        return ret;
+    }
+});
+var User = mongoose.model('User', userSchema);
+exports.default = User;
+//# sourceMappingURL=user.js.map
